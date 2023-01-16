ac85f6eb753c97bda46557b1c04f8aa044678aec
carrot <96284645+carrotshniper21@users.noreply.github.com>
2023-01-16 02:07:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 """
-For choosing provider and running the main function
+Main module for choosing a provider.
 """
-import sys
-from providers import zoro
-from providers import enime
-from providers import gogo
-from providers import pahe
+import getpass
+import subprocess
+from providers import zoro, enime, gogo, pahe
+from utils import util, arg_handler
+from utils.arg_handler import parse_args
 
-def choose_provider():
+class Provider:
     """
-    For choosing the provider the user wants
+    A class for choosing a provider.
     """
-    try:
-        all_providers = ["zoro", "animepahe", "enime", "gogoanime"]
-        print("[1] zoro\n[2] animepahe (slow)\n[3] enime\n[4] gogoanime\n")
-        chosen_provider = int(input("{+} Choose a provider 1-4: "))
-        return all_providers[chosen_provider - 1]
-    except ValueError:
-        print("{!} Enter a valid number")
-        sys.exit()
+    def __init__(self):
+        self.current_user = getpass.getuser()
+        self.all_providers = ["zoro", "animepahe", "enime", "gogoanime"]
+        self.default_provider = "zoro"
 
-if __name__ == "__main__":
-    provider = choose_provider()
+
+def main():
+    """
+    Passes the arguments to the chosen provider
+    would kinda look like this:
+        zoro.main(quality='auto', history=False, download=False, continue=False, config=False, auto_update=False, vlc=False, sources=False)
+    """
+    args = parse_args()
+
     providers = {
         "zoro": zoro.main,
         "animepahe": pahe.main,
         "enime": enime.main,
         "gogoanime": gogo.main,
     }
-    providers[provider]()
+    P = Provider()
+    provider = None
+    current_user = getpass.getuser()
+    if provider is None:
+        subprocess.run(["clear"])
+        print(util.colorcodes["Bold"] + f"Hello, {current_user}!\n" + util.colorcodes["Reset"])
+        print(util.colorcodes["Gray"] + "[*] This script is still in development so there will be some bugs!\nif you find any report them to: https://github.com/carrotshniper21/bc4e-cli" + util.colorcodes["Reset"])
+        print(util.colorcodes["Yellow"] + "[!] WARNING: " + util.colorcodes["Reset"] + "No provider chosen choosing default\n")
+        providers[P.default_provider](args)
+    elif provider is not None:
+        print(util.colorcodes["Bold"] + f"Hello, {current_user}!\n" + util.colorcodes["Reset"])
+        print(util.colorcodes["Gray"] + "[*] This script is still in development so there will be some bugs!\nif you find any report them to: https://github.com/carrotshniper21/bc4e-cli" + util.colorcodes["Reset"])
+        providers[provider](args)
 
+
+if __name__ == "__main__":
+    main()
