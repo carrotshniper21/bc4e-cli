f0aac01d53b8a3086842a4eaa445c6996803f587
carrot <96284645+carrotshniper21@users.noreply.github.com>
2023-01-16 01:33:15 -0600

    Add files via upload

---
+++ arg_handler.py
@@ -0,0 +1,67 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Watch anime from multiple providers or download it."
+    )
+
+    parser.add_argument(
+        "-q",
+        "--quality",
+        action="store",
+        required=False,
+        default="auto",
+    )
+    parser.add_argument(
+        "-H",
+        "--history",
+        required=False,
+        dest="history",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-d",
+        "--download",
+        required=False,
+        dest="download",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-C",
+        "--continue",
+        required=False,
+        dest="continue",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        required=False,
+        dest="config",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-a",
+        "--auto-update",
+        required=False,
+        dest="auto_update",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-v",
+        "--vlc",
+        required=False,
+        dest="vlc",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-s",
+        "--sources",
+        required=False,
+        dest="sources",
+        action="store_true",
+    )
+
+
+    return parser.parse_args()
